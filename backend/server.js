eb24f6ed73ad0ea141759b22836fc67cb66209c7
Quantum Cog <thequantumcog@protonmail.com>
2026-03-08 20:47:24 +0500

    divide server.js into files

--- backend/server.js
+++ backend/server.js
@@ -1,36 +1,12 @@
 const express = require("express")
-const sql = require("mssql")
 const cors = require("cors")
-require("dotenv").config({
-  path: '../.env'
-})
-
 const app = express()
+const db = require("./config/db.js")
+const authRoutes = require("./routes/authRoutes.js")
 app.use(cors())
+app.use(express.json())
+db.connect()
+app.use('/auth', authRoutes)
 
-const config = {
-  user: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  server: process.env.DB_SERVER,
-  database: process.env.DB_NAME,
-  port: parseInt(process.env.DB_PORT),
-  options: {
-    encrypt: false,
-    trustServerCertificate: true
-  }
-}
-sql.connect(config)
-  .then(() => console.log("✅ Connected to SQL Server successfully!"))
-  .catch(err => console.error("❌ Database connection failed:", err.message));
-app.get("/users", async (req, res) => {
-  try {
-    const result = await sql.query(`
-    Select * from UserAccount
-      `)
-    res.json(result.recordset)
-  } catch (err) {
-    res.status(500).send(err.message)
-  }
-})
 
 app.listen(3000, () => console.log("Backend running on port 3000"))
