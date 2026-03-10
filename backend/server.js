978873013ef343d1556f39eaa07554ac983263fc
Quantum Cog <thequantumcog@protonmail.com>
2026-03-10 17:19:14 +0500

    Add Cookies

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 const express = require("express")
 const cors = require("cors")
+const cookies = require("cookie-parser")
 const app = express()
 const db = require("./config/db.js")
 const authRoutes = require("./routes/authRoutes.js")
+
 app.use(cors())
+app.use(cookies())
 app.use(express.json())
 db.connect()
 app.use('/auth', authRoutes)
