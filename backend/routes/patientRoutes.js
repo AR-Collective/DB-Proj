ea1903aa262fabc3d0fecca10ea2c6da9a3ded36
Asjad-Raza-10 <syedasjadrazashirazi@gmail.com>
2026-03-27 02:40:19 +0500

    Disease-wise Patient Search

--- backend/routes/patientRoutes.js
+++ backend/routes/patientRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express'
-import { getCompatibleBlood } from '../controllers/patientController.js'
+import { getCompatibleBlood, searchByDisease } from '../controllers/patientController.js'
 import verifyToken from "../middleware/authMiddleware.js"
 
 const router = express.Router()
 
 router.use(verifyToken)
 router.post('/compatible-blood', getCompatibleBlood)
+router.post('/search-disease', searchByDisease)
 
 export default router
