996446c49bce14b37b1c4a1c2075a4bcdd627eb0
Asjad-Raza-10 <syedasjadrazashirazi@gmail.com>
2026-03-26 23:38:44 +0500

    Rate Donor Update donor rating

--- backend/routes/donorRoutes.js
+++ backend/routes/donorRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { searchDonors, getDonations } from '../controllers/donorController.js'
+import { searchDonors, getDonations, rateDonor } from '../controllers/donorController.js'
 import verifyToken from "../middleware/authMiddleware.js"
 
 const router = express.Router()
@@ -7,5 +7,6 @@ const router = express.Router()
 router.use(verifyToken)
 router.post('/search', searchDonors)
 router.post('/history', getDonations)
+router.post('/rate', rateDonor)
 
 export default router
