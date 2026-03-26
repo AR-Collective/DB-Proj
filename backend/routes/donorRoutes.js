455304ec788b7372bf60f52f7575e17f9693cac9
Syed Asjad Raza Shirazi <syedasjadrazashirazi@gmail.com>
2026-03-27 02:29:11 +0500

    Average Donations per Donor (#21)
    * Search Donor by Blood Group
    
    * Get Donor History
    View all donations by a donor (uses SELECT, JOIN)
    
    * Rate Donor
    Update donor rating
    
    * Get Blood Inventory by Location
    
    * Get Expiring Blood Units
    
    * Remove Expired Blood Units
    
    * Get Blood Demand by Type
    
    * Average Donations per Donor

--- backend/routes/donorRoutes.js
+++ backend/routes/donorRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { searchDonors, getDonations, rateDonor } from '../controllers/donorController.js'
+import { searchDonors, getDonations, rateDonor, getAverageDonations } from '../controllers/donorController.js'
 import verifyToken from "../middleware/authMiddleware.js"
 
 const router = express.Router()
@@ -8,5 +8,6 @@ router.use(verifyToken)
 router.post('/search', searchDonors)
 router.post('/history', getDonations)
 router.post('/rate', rateDonor)
+router.get('/statistics/average', getAverageDonations)
 
 export default router
