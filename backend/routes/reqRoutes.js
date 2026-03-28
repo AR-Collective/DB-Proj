4b073431776d16e22583eeff3ef487b5a447fc30
rookiecoder231 <shaheer_csbc@hotmail.com>
2026-03-28 20:01:21 +0500

    Add route for getRequestsByHospital

--- backend/routes/reqRoutes.js
+++ backend/routes/reqRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express'
-import { insertBloodRequest, getBloodUnits } from '../controllers/BReqController.js'
+import { insertBloodRequest, getBloodUnits, getRequestsByHospital} from '../controllers/BReqController.js'
 import verifyToken from "../middleware/authMiddleware.js"
 
 const router = express.Router()
 router.use(verifyToken)
 router.post('/insertBReq', insertBloodRequest)
 router.get('/getBU', getBloodUnits)
+router.get('/getReqByHospital', getRequestsByHospital)
 
 export default router
