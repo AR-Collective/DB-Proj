0d2cd8d61084bd983e4e9088d55549aa34866e9c
Syed Asjad Raza Shirazi <syedasjadrazashirazi@gmail.com>
2026-05-02 01:18:51 +0500

    Integrate Postgres functions in Models (#46)
    * Get Expiring Units
    
    * donor.js models db
    
    * hospital.js models db
    
    * inventory.js models DB
    
    * patient.js models DB
    
    * testing.js models DB

--- backend/models/testing.js
+++ backend/models/testing.js
@@ -2,20 +2,7 @@ import db from '../config/db.js';
 
 const getTestResultsForDonations = async () => {
     try {
-        const query = `
-            SELECT tr.TestID, tr.DonationID, d.DonorID,
-                   u.FirstName as DonorFirstName, u.LastName as DonorLastName, d.DonationDate,
-                   b.BloodType, tr.ScreeningStatus, tr.DiseaseIndicators, tr.TestDate
-            FROM TestResult tr
-            JOIN Donation d ON tr.DonationID = d.DonationID
-            JOIN Donor donor ON d.DonorID = donor.DonorID
-            JOIN UserAccount u ON donor.DonorID = u.UserID
-            JOIN BloodGroup b ON donor.BloodGroupID = b.BloodGroupID
-            LEFT JOIN Patient p ON tr.PatientID = p.PatientID
-            ORDER BY tr.TestDate DESC
-        `;
-
-        return await db.query(query);
+        return await db.query('SELECT * FROM vw_comprehensive_test_log');
     } catch (err) {
         throw err;
     }
