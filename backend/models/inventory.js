05d1d39360b636f3e97f308d1d4294bb40306ef1
Asjad-Raza-10 <syedasjadrazashirazi@gmail.com>
2026-05-02 01:12:24 +0500

    inventory.js models DB

--- backend/models/inventory.js
+++ backend/models/inventory.js
@@ -2,17 +2,7 @@ import db from '../config/db.js';
 
 const getInventoryByLocation = async () => {
     try {
-        const query = `
-            SELECT sl.LocationID, sl.LocationName, sl.Address, sl.Capacity,
-                   b.BloodType, SUM(bu.Quantity) as TotalUnits, COUNT(bu.UnitID) as NumberOfUnits
-            FROM StorageLocation sl
-            JOIN BloodUnit bu ON sl.LocationID = bu.LocationID
-            JOIN BloodGroup b ON bu.BloodGroupID = b.BloodGroupID
-            GROUP BY sl.LocationID, sl.LocationName, sl.Address, sl.Capacity, b.BloodType
-            ORDER BY sl.LocationName, b.BloodType
-        `;
-
-        return await db.query(query);
+        return await db.query('SELECT * FROM vw_inventory_by_location');
     } catch (err) {
         throw err;
     }
@@ -28,12 +18,7 @@ const getExpiringUnits = async (days) => {
 
 const removeExpiredUnits = async () => {
     try {
-        const query = `
-            DELETE FROM BloodUnit
-            WHERE ExpiryDate < CURRENT_DATE
-        `;
-
-        return await db.query(query);
+        return await db.query('CALL sp_purge_expired_units()');
     } catch (err) {
         throw err;
     }
@@ -41,16 +26,7 @@ const removeExpiredUnits = async () => {
 
 const getBloodDemandByType = async () => {
     try {
-        const query = `
-            SELECT b.BloodType, COUNT(br.RequestID) as DemandCount
-            FROM BloodRequest br
-            JOIN BloodGroup b ON br.BloodGroupID = b.BloodGroupID
-            GROUP BY b.BloodType, b.BloodGroupID
-            HAVING COUNT(br.RequestID) > 0
-            ORDER BY DemandCount DESC
-        `;
-
-        return await db.query(query);
+        return await db.query('SELECT * FROM vw_blood_demand_analytics');
     } catch (err) {
         throw err;
     }
@@ -58,25 +34,7 @@ const getBloodDemandByType = async () => {
 
 const getBloodAvailabilityReport = async () => {
     try {
-        const query = `
-            SELECT b.BloodType, SUM(bu.Quantity) as TotalQuantity, bu.Status
-            FROM BloodUnit bu
-            JOIN BloodGroup b ON bu.BloodGroupID = b.BloodGroupID
-            WHERE bu.Status = 'Available'
-            GROUP BY b.BloodType, bu.Status
-            
-            UNION
-            
-            SELECT b.BloodType, SUM(bu.Quantity) as TotalQuantity, bu.Status
-            FROM BloodUnit bu
-            JOIN BloodGroup b ON bu.BloodGroupID = b.BloodGroupID
-            WHERE bu.Status = 'Reserved'
-            GROUP BY b.BloodType, bu.Status
-            
-            ORDER BY BloodType, Status
-        `;
-
-        return await db.query(query);
+        return await db.query('SELECT * FROM vw_availability_report');
     } catch (err) {
         throw err;
     }
