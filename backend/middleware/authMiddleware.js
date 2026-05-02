89daae48d1a7e87e38bb4261956c5c425d4a7d94
Quantum Cog <thequantumcog@protonmail.com>
2026-05-02 15:42:20 +0500

    Allow specific roles to access specific routes

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,6 +1,26 @@
 import jwt from 'jsonwebtoken'
 
-function verifyToken(req, res, next) {
+export const authorizeRoles = (...allowedRoles) => {
+	return (req, res, next) => {
+		if (!req.user || !req.user.role) {
+			return res.status(401).json({
+				success: false,
+				message: "Unauthorized. Please log in first."
+			});
+		}
+
+		if (!allowedRoles.includes(req.user.role)) {
+			return res.status(403).json({
+				success: false,
+				message: `Forbidden. Your role (${req.user.role}) does not have access to this resource.`
+			});
+		}
+
+		next();
+	};
+};
+
+export function attachToken(req, res, next) {
 	// ? here means if authorization doesnt exists in header then dont try to split
 	const token = req.headers['authorization']?.split(' ')[1]
 
@@ -15,4 +35,14 @@ function verifyToken(req, res, next) {
 	})
 }
 
-export default verifyToken
+export function verifyToken(req, res, next) {
+	const cookie = req.cookies.auth_token;
+	if (cookie) {
+		jwt.verify(cookie, process.env.JWT_SECRET, (err, decoded) => {
+			if (err) return res.status(403).json({ error: 'Invalid token' })
+			req.user = decoded
+			next()
+		})
+	}
+}
+export default attachToken
